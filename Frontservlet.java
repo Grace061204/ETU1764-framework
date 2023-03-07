e1051809564a5a305258d14829faba6cbc8f99ec
Grace061204 <grace.andrianisoa@gmail.com>
2023-03-07 08:38:24 +0100

    sprint 2 done

--- Frontservlet.java
+++ Frontservlet.java
@@ -1,6 +1,9 @@
 package etu1764.framework.servlet;
 
 import java.io.*;
+import java.util.HashMap;
+
+import etu1764.framework.Mapping;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.ServletException;
@@ -10,13 +13,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import traitement.Util;
 
 public class Frontservlet extends HttpServlet {
+    HashMap<String,Mapping> mappingUrls = new HashMap<>();
     Util util = new Util();
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         out.println("okayyy");
         String url = request.getRequestURI();
         String traitUrl = util.url(url);
         out.println("traitUrl = "+traitUrl);
+
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
         processRequest(request, response);
